bcaaf4d4308ab28163480d8287a580d6e1ed04fb
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-03-29 18:41:36 +0100

    Add a utility class

---
+++ R/sources.R
@@ -0,0 +1,17 @@
+conan_sources <- function(packages, repos = NULL, cran = NULL) {
+  ## Standardise references as this will make working with them later
+  ## easier:
+  for (i in seq_dat(packages)) {
+    dat <- pkgdepends::parse_pkg_ref(packages[[i]])
+    if (dat$type == "local" && !grepl("^local::", packages[[i]])) {
+      if (!file.exists(dat$path)) {
+        stop(sprintf("Local package source '%s' does not exist", dat$path))
+      }
+      packages[[i]] <- sprintf("local::%s", packages[[i]])
+    }
+  }
+  ret <- list(packages = packages,
+              repos = clean_repos(repos, cran))
+  class(ret) <- "conan_sources"
+  ret
+}
