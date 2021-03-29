6b13b56f959cd9553f054c9cc298cc4c1f02bb12
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-03-29 13:27:59 +0100

    Add integration test

--- tests/testthat/helper-conan.R
+++ tests/testthat/helper-conan.R
@@ -1,3 +1,12 @@
 clean_paths <- function(p) {
   vapply(p, normalizePath, "", USE.NAMES = FALSE)
 }
+
+
+skip_unless_ci <- function() {
+  testthat::skip_on_cran()
+  if (isTRUE(as.logical(Sys.getenv("CI")))) {
+    return(invisible(TRUE))
+  }
+  testthat::skip("Not on CI")
+}
---
+++ tests/testthat/test-zzz-integration.R
@@ -0,0 +1,54 @@
+context("integration")
+
+## This one is quite slow so we'll need to mostly mock out the bits of it
+test_that("Can create bootstrap library", {
+  testthat::skip_if_offline()
+  skip_unless_ci()
+
+  path_scripts <- tempfile()
+  conan_scripts(path_scripts)
+  path_bootstrap <- tempfile()
+
+  pkgs <- c("docopt", "pkgcache", "pkgdepends")
+  expect_setequal(missing_packages(pkgs, path_bootstrap), pkgs)
+
+  callr::rscript(
+    c("--vanilla", file.path(path_scripts, "conan-bootstrap")),
+    path_bootstrap,
+    echo = FALSE,
+    show = FALSE)
+
+  expect_true(
+    all(pkgs %in% .packages(TRUE, path_bootstrap)))
+  expect_equal(missing_packages(pkgs, path_bootstrap), character(0))
+})
+
+
+test_that("Can install packages", {
+  testthat::skip_if_offline()
+  skip_unless_ci()
+
+  path_bootstrap <- .libPaths()[[1]]
+  expect_equal(
+    missing_packages(c("docopt", "pkgcache", "pkgdepends"), path_bootstrap),
+    character(0))
+
+  path_scripts <- tempfile()
+  conan_scripts(path_scripts)
+
+  path_plan <- tempfile()
+  conan_write_plan(path_plan,
+                   c("cpp11", "dde"),
+                   "https://mrc-ide.github.io/drat")
+
+  path_lib <- tempfile()
+
+  env <- c(callr::rcmd_safe_env(),
+           "CONAN_PATH_BOOTSTRAP" = path_bootstrap)
+  callr::rscript(
+    c("--vanilla", file.path(path_scripts, "conan-install"),
+      path_lib, path_plan),
+    echo = TRUE, env = env)
+
+  expect_true(all(c("cpp11", "dde", "ring") %in% dir(path_lib)))
+})
